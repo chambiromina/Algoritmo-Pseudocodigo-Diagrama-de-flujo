194c33a3c6552d3f6a52ecc5c10b80e92dc2cd9f
Romina <chambiromina4@gmail.com>
2024-09-04 14:24:58 -0300

    se agrega algoritmo

---
+++ conversion-moneda.py
@@ -0,0 +1,25 @@
+# Paso 1: Definir el Valor Actual del Euro y Dolar con respecto al Peso Mexicano
+ 
+tipo_cambio_eur_a_mxn = 23.70
+tipo_cambio_usd_a_mxn = 20.75
+
+#Paso 2: Solicitar al usuario el tipo de conversion (Euro a Mex o Dolar a Max)
+
+tipo_conversion = input ("Ingrese la moneda origen para la conversion (EUR/USD): ")
+
+#Paso 3:Solicitar al usuario el monto a convertir
+
+monto_a_convertir = float (input("Ingrese el monto a convertir: "))
+
+
+#Paso 4: Realizar la conversion utilizando el tipo de cambio correspondiente
+#Paso 5: Mostrar el resultado de la conversion al usuario
+
+if tipo_conversion.upper() == "EUR":
+    resultado = monto_a_convertir * tipo_cambio_eur_a_mxn
+    print("El resultado de la conversion de EUR a MXN es: ", resultado)
+elif tipo_conversion.upper() == "USD":
+    resultado = monto_a_convertir * tipo_cambio_usd_a_mxn
+    print("El resultado de la conversion de USD a MXN es: ", resultado)
+else:
+    print("Tipo de conversion no valido")
